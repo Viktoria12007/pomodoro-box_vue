b8e91c4c96b6930d82cb3bf7bd84653ff7a4534a
Виктория Голубова <v.golubova@denexy.com>
2024-03-21 10:00:56 +0400

    Created views components - MainView.vue and StatisticsView.vue

---
+++ src/views/MainView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import Description from '../components/Description.vue'
+import AddTaskForm from '../components/AddTaskForm.vue'
+import ListTasks from '../components/ListTasks.vue'
+import Timer from '../components/Timer.vue'
+</script>
+
+<template>
+  <div class='main__wrap'>
+    <div class='main__left-column'>
+      <Description/>
+      <AddTaskForm/>
+      <ListTasks/>
+    </div>
+    <Timer />
+  </div>
+</template>
+
+<style scoped>
+.main__wrap {
+  display: flex;
+  gap: 16px;
+  /*height: calc(100vh - 75px);*/
+  padding: 175px 0 100px 0;
+}
+/*.main__wrap > * {*/
+/*  width: 50%;*/
+/*}*/
+.main__left-column {
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+}
+</style>
---
+++ src/views/StatisticsView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Статистика</div>
+</template>
+
+<style></style>
