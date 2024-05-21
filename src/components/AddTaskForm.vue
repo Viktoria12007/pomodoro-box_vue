ce5dcb2bd75ed7c5f3882965bd7b0d3ac32332c7
Виктория Голубова <v.golubova@denexy.com>
2024-05-21 10:54:21 +0400

    Created submit and validation methods for AddTaskForm.vue

--- src/components/AddTaskForm.vue
+++ src/components/AddTaskForm.vue
@@ -1,14 +1,44 @@
 <script setup lang="ts">
 import Button from '../components/UI/Button.vue'
-function addTask() {
-  console.log('addTask');
+import { useTasksListStore } from '@/stores/tasksList'
+import { computed, ref, watch } from 'vue'
+
+const store = useTasksListStore()
+const { addTask } = store
+const nameTask = ref('')
+const resolveValidation = ref(false)
+
+function handleAddTask(e) {
+  addTask({
+    id: self.crypto.randomUUID(),
+    name: nameTask.value,
+    pomodoro: 1,
+  })
+  e.target.reset()
 }
+const validation = computed(() => {
+  if (nameTask.value) {
+    if (nameTask.value.length > 0 && nameTask.value.length < 2) {
+      return 'Название задачи должно содержать минимум 2 символа!'
+    } else if (nameTask.value.length > 40) {
+      return 'Название задачи должно содержать максимум 40 символов!'
+    }
+    return ''
+  }
+  return 'Введите название задачи!'
+})
+watch(nameTask, () => {
+  resolveValidation.value = true
+}, {
+  once: true
+})
 </script>
 
 <template>
-  <form @submit.prevent='addTask' class='add-task-form'>
-    <input placeholder='Название задачи' class='add-task-form__input'/>
-    <Button type='submit'>Добавить</Button>
+  <form @submit.prevent='handleAddTask' class='add-task-form'>
+    <input v-model.trim='nameTask' placeholder='Название задачи' class='add-task-form__input'/>
+    <div v-show='validation && resolveValidation' class='add-task-form__validation'>{{validation}}</div>
+    <Button type='submit' :disabled='!!validation'>Добавить</Button>
   </form>
 </template>
 
@@ -16,6 +46,7 @@ function addTask() {
 .add-task-form {
   width: 370px;
   display: flex;
+  position: relative;
   flex-direction: column;
   align-items: flex-start;
   margin-bottom: 25px;
@@ -30,5 +61,10 @@ function addTask() {
 }
 .add-task-form__input::placeholder {
   color: var(--gray-dark);
+}
+.add-task-form__validation {
+  position: absolute;
+  top: 55px;
+  color: var(--red);
 }
 </style>
