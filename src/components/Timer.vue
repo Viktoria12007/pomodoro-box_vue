ba0f481cfef033202b5137e128ea0486809f665f
Виктория Голубова <v.golubova@denexy.com>
2024-05-21 10:57:59 +0400

    Added dynamic layout task`s data with animation.

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
 import Button from '@/components/UI/Button.vue'
 import IconIncreaseTime from '@/components/icons/IconIncreaseTime.vue'
+import { useTasksListStore } from '@/stores/tasksList'
+import { storeToRefs } from 'pinia'
+import { usePomodoroStore } from '@/stores/pomodoro'
+import { ref } from 'vue'
 
+const storeListTasks = useTasksListStore()
+const storePomodoro = usePomodoroStore()
+const { firstTask } = storeToRefs(storeListTasks)
+const { pomodoro } = storeToRefs(storePomodoro)
+let seconds = ref(0)
+setInterval(() => {
+  seconds.value++
+}, 1000)
 function handleStart() {
   console.debug('start');
 }
@@ -13,24 +25,30 @@ function handleStop() {
 <template>
   <div class='timer'>
     <div class='timer__header'>
-      <div class='timer__header-task'>Сверстать сайт </div>
-      <div>Помидор 1</div>
+      <div class='timer__header-task'>{{ firstTask?.name || '' }}</div>
+      <div>{{ `Помидор ${firstTask?.pomodoro}` || '' }}</div>
     </div>
     <div class='timer__content'>
-      <div class='timer__time-wrap'>
-        <div class='timer__time'>25:00</div>
-        <Button class='timer__increase-button' variant='light'>
-          <IconIncreaseTime class='timer__icon'/>
-        </Button>
-      </div>
-      <div class='timer__task'>
-        <span class='timer__task_gray'>Задача 1 - </span>
-        <span>Сверстать сайт </span>
-      </div>
-      <div class='timer__action-wrap'>
-        <Button @click='handleStart'>Старт</Button>
-        <Button @click='handleStop' variant='outline'>Стоп</Button>
-      </div>
+      <template v-if='firstTask'>
+        <div class='timer__time-wrap'>
+<!--          <div class='timer__time'>{{firstTask.pomodoro * pomodoro}}</div>-->
+          <Transition mode='out-in'>
+            <div class='timer__time' :key={seconds}>{{seconds}}</div>
+          </Transition>
+          <Button class='timer__increase-button' variant='light'>
+            <IconIncreaseTime class='timer__icon'/>
+          </Button>
+        </div>
+        <div class='timer__task'>
+          <span class='timer__task_gray'>Задача 1 - </span>
+          <span>{{firstTask.name}}</span>
+        </div>
+        <div class='timer__action-wrap'>
+          <Button @click='handleStart'>Старт</Button>
+          <Button @click='handleStop' variant='outline'>Стоп</Button>
+        </div>
+      </template>
+      <div v-else>Задач пока нет!</div>
     </div>
   </div>
 </template>
@@ -88,5 +106,13 @@ function handleStop() {
 .timer__action-wrap {
   display: flex;
   gap: 25px;
+}
+.v-enter-active,
+.v-leave-active {
+  transition: opacity .4s ease-in-out;
+}
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
