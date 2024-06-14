1a8a7bcf98de3e5698ae30b02aab006dbea39c1d
Виктория Голубова <v.golubova@denexy.com>
2024-06-14 13:35:00 +0400

    Implemented logic for methods: deleteTask, orderSelectTaskForTimer, selectTaskForTimer, selectTaskForAction, setShowEditTaskInput.

--- src/stores/tasksList.ts
+++ src/stores/tasksList.ts
@@ -1,45 +1,61 @@
 import { computed, reactive, ref } from 'vue'
 import { defineStore } from 'pinia'
 import type { TasksList, Task } from '@/types/types'
-import { usePomodoroStore } from '@/stores/pomodoro'
+import { useSettingsStore } from '@/stores/settings'
 
 export const useTasksListStore = defineStore('tasksList', () => {
   const tasks = reactive<TasksList>(localStorage.pomodoroTasks ? JSON.parse(localStorage.pomodoroTasks) : [])
-  const selectedTask = ref<Task | null>(null)
+  const selectedTaskForTimer = ref<Task | null>(tasks[0] ? tasks[0] : null)
+  const selectedTaskForAction = ref(null)
+  const showEditTaskInput = ref(false)
   const lengthTasks = computed(() => tasks.length)
   const totalTime = computed(() => {
-    const pomodoroStore = usePomodoroStore()
-    return tasks.reduce((time, task) => time + task.pomodoro, 0) * pomodoroStore.$state.pomodoro
+    const pomodoroStore = useSettingsStore()
+    return tasks.reduce((time, task) => time + task.pomodoro, 0) * pomodoroStore.$state.pomodoroTime
   })
-  const firstTask = computed<Task>(() => tasks[0])
+  const firstTask = computed<Task>(() => tasks[0]) // удалить?
+  const orderSelectTaskForTimer = computed<number>(() => tasks.findIndex((task) => task.id === selectedTaskForTimer.value?.id) + 1)
 
-  function selectTask(task: Task) {
-    selectedTask.value = task
+  function selectTaskForTimer(task: Task) {
+    selectedTaskForTimer.value = task
+  }
+  function selectTaskForAction(action) {
+    selectedTaskForAction.value = action
+  }
+  function setShowEditTaskInput(show) {
+    showEditTaskInput.value = show;
   }
   function addTask(task: Task) {
     tasks.push(task)
   }
-  function deleteTask() {
-    console.debug('deleteTask')
-    const indexTask = tasks.findIndex((item) => item.id === selectedTask.value.id)
+  function deleteTask(goal: string = 'action') {
+    const currentGoal = goal === 'action' ? selectedTaskForAction.value.id : selectedTaskForTimer.value.id
+    const indexTask = tasks.findIndex((item) => item.id === currentGoal)
     tasks.splice(indexTask, 1)
   }
-  function editTask(key: string, value: string | number) {
-    console.debug('editTask')
-    const currentTask = tasks.find((item) => item.id === selectedTask.value.id)
-    if (value === 'increase') {
-      currentTask[key]++
-    } else if (value === 'decrease' && currentTask[key] > 1) {
-      currentTask[key]--
+  function editTask(property: string, action: string, newName: string = '', goal: string = 'action') {
+    const currentGoal = goal === 'action' ? selectedTaskForAction.value.id : selectedTaskForTimer.value.id
+    const currentTask = tasks.find((item) => item.id === currentGoal)
+    if (action === 'increase') {
+      currentTask[property]++
+    } else if (action === 'decrease' && currentTask[property] > 1) {
+      currentTask[property]--
+    } else if (action === 'editName') {
+      currentTask[property] = newName
     }
   }
   return {
     tasks,
-    selectedTask,
+    selectedTaskForTimer,
+    selectedTaskForAction,
+    showEditTaskInput,
     lengthTasks,
     totalTime,
     firstTask,
-    selectTask,
+    orderSelectTaskForTimer,
+    selectTaskForTimer,
+    selectTaskForAction,
+    setShowEditTaskInput,
     addTask,
     deleteTask,
     editTask
