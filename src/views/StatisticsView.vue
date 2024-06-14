4e5c6b59ef26e7a12a5b5091060189e986ab97e3
Виктория Голубова <v.golubova@denexy.com>
2024-06-14 13:18:00 +0400

    Added StatisticsHeader.vue and Dashboard.vue to StatisticsView.vue

--- src/views/StatisticsView.vue
+++ src/views/StatisticsView.vue
@@ -1,5 +1,17 @@
+<script setup lang='ts'>
+import StatisticsHeader from '../components/StatisticsHeader.vue'
+import Dashboard from '../components/Dashboard.vue'
+</script>
+
 <template>
-  <div>Статистика</div>
+  <div class='statistics__wrap'>
+    <StatisticsHeader/>
+    <Dashboard/>
+  </div>
 </template>
 
-<style></style>
+<style scoped>
+.statistics__wrap {
+  padding: 158px 0 99px 0;
+}
+</style>
