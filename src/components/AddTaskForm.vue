d9a8f56b5e43b837ba80d2ec46ec68a3392251d3
Виктория Голубова <v.golubova@denexy.com>
2024-06-20 22:53:40 +0400

    Added check for emptiness nameTask. Added attributes for <Input/>.

--- src/components/AddTaskForm.vue
+++ src/components/AddTaskForm.vue
@@ -2,71 +2,65 @@
 import Button from '../components/UI/Button.vue'
 import Input from '../components/UI/Input.vue'
 import { useTasksListStore } from '@/stores/tasksList'
-import { computed, ref, watch } from 'vue'
+import { nextTick, ref, watch } from 'vue'
+import { useValidationInput } from '@/hooks/useValidationInput'
 
 const store = useTasksListStore()
 const { addTask } = store
+
 const nameTask = ref('')
 const resolveValidation = ref(false)
+const { validation, setValidation, validationNameTaskInput } = useValidationInput()
 
-function handleAddTask(e) {
-  addTask({
-    id: self.crypto.randomUUID(),
-    name: nameTask.value,
-    pomodoro: 1,
-  })
-  nameTask.value = ''
-  resolveValidation.value = false
-  // e.target.reset()
-}
-const validation = computed(() => {
+async function handleAddTask() {
   if (nameTask.value) {
-    if (nameTask.value.length > 0 && nameTask.value.length < 2) {
-      return 'Название задачи должно содержать минимум 2 символа!'
-    } else if (nameTask.value.length > 40) {
-      return 'Название задачи должно содержать максимум 40 символов!'
-    }
-    return ''
+    addTask({
+      id: self.crypto.randomUUID(),
+      name: nameTask.value,
+      pomodoro: 1,
+      currentPomodoro: 1,
+    })
+    nameTask.value = ''
+    await nextTick()
+    resolveValidation.value = false
   }
-  return 'Введите название задачи!'
-})
+}
+
 watch(nameTask, () => {
   resolveValidation.value = true
-}, {
-  once: true
 })
 </script>
 
 <template>
   <form @submit.prevent='handleAddTask' class='add-task-form'>
     <Input
-      v-model='nameTask'
-      :validation='validation'
-      :resolveValidation='resolveValidation'
+      v-model.trim='nameTask'
+      type='text'
+      minlength='2'
+      maxlength='40'
       placeholder='Название задачи'
       class='add-task-form__input'
+      @input='(e) => setValidation(validationNameTaskInput(e.target.value))'
+      :validation='validation'
+      :resolveValidation='resolveValidation'
     />
-<!--    <div v-show='validation && resolveValidation' class='add-task-form__validation'>{{validation}}</div>-->
     <Button type='submit' :disabled='!!validation'>Добавить</Button>
   </form>
 </template>
 
+<style>
+.add-task-form__input {
+  width: 100%;
+  padding: 19px 15px;
+  margin-bottom: 35px;
+}
+</style>
 <style scoped>
 .add-task-form {
   width: 370px;
   display: flex;
-  position: relative;
   flex-direction: column;
   align-items: flex-start;
   margin-bottom: 25px;
 }
-.add-task-form__input {
-  padding: 19px 15px;
-  margin-bottom: 35px;
-}
-/*.add-task-form__validation {*/
-/*  position: absolute;*/
-/*  top: 55px;*/
-/*  color: var(--red);*/
-/*}*/
 </style>
