de8e0edd1e32d013ace941ef6a7ed9ca8d0ae906
Виктория Голубова <v.golubova@denexy.com>
2024-06-20 22:36:11 +0400

    Added properties into store settings.ts

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -2,24 +2,43 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useSettingsStore = defineStore('settings', () => {
-  const pomodoroTime = ref(1)
-  const breakTime = ref(1)
-  const pomodoro = ref(0)
+  const pomodoroTime = ref<number>(1) // 25
+  const shortBreakTime = ref<number>(1) // 5
+  const longBreakTime = ref<number>(2) // 15
+  const frequencyLongBreak = ref<number>(2) // 4
+  const onNotifications = ref<boolean>(true) // true
+  // const pomodoro = ref(0)
   function setPomodoroTime(time: number) {
     pomodoroTime.value = time
   }
-  function setBreakTime(time: number) {
-    breakTime.value = time
+  function setShortBreakTime(time: number) {
+    shortBreakTime.value = time
   }
-  function setPomodoro(quantity: number) {
-    pomodoro.value = quantity
+  function setLongBreakTime(time: number) {
+    longBreakTime.value = time
   }
+  function setFrequencyLongBreak(time: number) {
+    frequencyLongBreak.value = time
+  }
+  function setOnNotifications(flag: boolean) {
+    console.log(flag)
+    onNotifications.value = flag
+  }
+  // function setPomodoro(quantity: number) {
+  //   pomodoro.value = quantity
+  // }
   return {
     pomodoroTime,
     setPomodoroTime,
-    breakTime,
-    setBreakTime,
-    pomodoro,
-    setPomodoro,
+    shortBreakTime,
+    setShortBreakTime,
+    longBreakTime,
+    setLongBreakTime,
+    frequencyLongBreak,
+    setFrequencyLongBreak,
+    onNotifications,
+    setOnNotifications
+    // pomodoro,
+    // setPomodoro,
   }
 })
