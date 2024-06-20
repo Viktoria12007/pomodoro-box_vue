27af50147b67837b116ea98f12edf2ba20707824
Виктория Голубова <v.golubova@denexy.com>
2024-06-20 22:21:09 +0400

    Changed icon IconChart.vue

--- src/components/icons/IconChart.vue
+++ src/components/icons/IconChart.vue
@@ -1,12 +1,5 @@
 <template>
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g clip-path="url(#clip0_27380_196)">
-      <path d="M10 20H14V4H10V20ZM4 20H8V12H4V20ZM16 9V20H20V9H16Z" />
-    </g>
-    <defs>
-      <clipPath id="clip0_27380_196">
-        <rect width="24" height="24" fill="white"/>
-      </clipPath>
-    </defs>
+  <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 16H10V0H6V16ZM0 16H4V8H0V16ZM12 5V16H16V5H12Z"/>
   </svg>
 </template>
