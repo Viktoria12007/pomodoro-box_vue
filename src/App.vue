5d57c105a999904fcfff8895c2aa5aa90b6560b0
Виктория Голубова <v.golubova@denexy.com>
2024-06-20 22:18:46 +0400

    Added keep-alive into router-view.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
 import Header from '@/components/Header.vue'
 </script>
 
 <template>
   <Header />
   <main class='main container'>
-    <RouterView />
+    <router-view v-slot="{ Component }">
+      <keep-alive>
+        <component :is="Component" />
+      </keep-alive>
+    </router-view>
   </main>
 </template>
 
